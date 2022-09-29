66914cb2d0efdf02a5d3b00da0df04fbb9d2826c
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2022-09-30 01:57:42 +0300

    Дописать метод для нахождения количество товаров, которые по цене в указанном диапазоне.

--- pr3/main.py
+++ pr3/main.py
@@ -4,6 +4,7 @@
 import codecs
 
 file1 = codecs.open("C:\\Users\\User\\Desktop\\ТРПО\\Практика 3\\9901\\test.txt", encoding='utf-8')
+count = 0
 
 while True:
     line = file1.readline()
@@ -11,6 +12,7 @@ while True:
     if not line:
         break
     if (300 <= int(mas[2]) <= 1100):
+        count += 1
         print(mas[0])
-
+print("Количество товара по цене в диапазоне = " + str(count))
 file1.close
